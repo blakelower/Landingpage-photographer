d8280f35fc70b77e25a1f6cf1fee14be0a22c4db
blakelower <blakelower1999@gmail.com>
2020-07-19 16:11:01 -0700

    finished

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "./oj1.jpg",
+    image2: "./oj2.jpg",
+    displacementImage: "./diss.png"
+});
